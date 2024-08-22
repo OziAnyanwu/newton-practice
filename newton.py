14e7c0553bc15537e5e6b2ea74131748a18b3873
Ozi Anyanwu <ozijesus@stanford.edu>
2024-08-22 11:50:39 -0700

    clarity

--- newton.py
+++ newton.py
@@ -6,11 +6,18 @@ def derivative(f,n):
         return lambda x: (f(x+EPSILON) - f(x)) / EPSILON
     return derivative(derivative(f,n-1), 1)
 
-def optimize(f, x):
+def optimize(f, x_0):
+    x = x_0
     for _ in range(1000):
         x_new = x - derivative(f,1)(x) / derivative(f,2)(x)
         if abs(x_new - x) < EPSILON:
-            return x_new
+            return x_new, f(x)
         x = x_new
     print('Didnt find minimum after 1000 runs')
-    return x
+    return x, f(x)
+
+def f(x):
+    return x**2 - 5
+
+x_opt, opt = optimize(f, 17.5)
+print(f"Minimum: {opt} at x = {x_opt}")
