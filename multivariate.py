d0e0ad2c383acc6114de28814ab7a4fcaec6a21b
Ozi Anyanwu <ozijesus@stanford.edu>
2024-08-23 09:52:19 -0700

    multivariate branch]

--- multivariate.py
+++ multivariate.py
@@ -1,11 +1,9 @@
+import numpy as np
+import numdifftools as nd
+from scipy.linalg import inv
 EPSILON = 1e-6
 
 
-# Returns a function representing the nth derivative of f
-def derivative(f, n):
-    if n == 1:
-        return lambda x: (f(x + EPSILON) - f(x)) / EPSILON
-    return derivative(derivative(f, n - 1), 1)
 
 def optimize(f, x_0):
     """
@@ -15,10 +13,15 @@ def optimize(f, x_0):
     """
     x = x_0
     for _ in range(1000):
-        x_new = x - derivative(f, 1)(x) / derivative(f, 2)(x)
-        if abs(x_new - x) < EPSILON:
+        x_new = x - np.dot(inv(nd.Hessian(f)(x)), nd.Gradient(f)(x))
+        if np.linalg.norm(x_new - x) < EPSILON:
             return  {'x': x_new, 'f(x)': f(x_new)}
         x = x_new
 
     print("Didnt find minimum after 1000 runs")
     return {'x': x, 'f(x)': f(x)}
+
+def f(x):
+    return np.sum(np.array(x)**2) - 5
+
+print(optimize(f,[8,8]))
